e3743cb99a5eb859786be31454ccab2d1aef9a16
Soner Şensoy <soner.shensoy@gmail.com>
2024-07-16 23:13:04 +0300

    Add logo and main components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import React from "react"
+import ReactDOM from "react-dom/client";
 import './App.css';
+import Header from "./components/Header.js"
+import Main from "./components/Main.js"
+import Footer from"./components/Footer.js"
+import Nav from"./components/Nav.js"
 
 function App() {
   return (
-    <div className="App">
-     Homepage
-    </div>
+    <>
+     <Header />
+     <Nav />
+     <Main />
+     <Footer />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import logo from "../icons_assets/Logo.svg"
+
+export default function Header() {
+    return (
+        <header>
+            <img src={logo} alt="" />
+       
+        </header>
+    )
+}
+
